2abbe63f92f99564b441d5eed591a93bb53c8c65
NicoPr <niccolo@red7mobile.com>
2019-03-06 17:54:35 +0000

    partially reformat

--- crawlers/skybetCrawler.js
+++ crawlers/skybetCrawler.js
@@ -2,18 +2,14 @@ const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 
 module.exports = {
-
 	run: async () => {
 
 		const browser = await puppeteer.launch({ headless: true});
 		const page = await browser.newPage();
 		page.setViewport({width: 1700, height: 1300});
 
-		const baseURL = "https://m.skybet.com";
+		const baseURL = 'https://m.skybet.com/';
 		await page.goto(`${baseURL}/esports`);
-		// await page.waitForSelector('body.blog');
-		await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'})
-		const csgoSelector = ' #page-content > .table-group > [data-ui-state=esportsfeatured] ';
 
 		//get the entire dom as text
 		let allDom = await page.evaluate((sel) => {
@@ -21,14 +17,14 @@ module.exports = {
 			return element ? element.innerHTML : null;
 		}, "body");
 
+		const $ = cheerio.load(allDom)
+		const eventDataSelector = ' #page-content > .table-group > [data-ui-state=esportsfeatured] ';
+		const matchDataSelector = '.accordion--generic > table > tbody';
 		const data = [];
 
+		$(eventDataSelector).children().each((i, elem) => { //gets the events
 
-		const $ = cheerio.load(allDom)
-		$(csgoSelector).children().each((i, elem) => {
-			console.log(i)
-
-			const eventName = $(elem).find('.accordion--generic > table > tbody').children().each((i,elem) => {
+			$(elem).find(matchDataSelector).children().each((i,elem) => { 	//gets the matches
 				let eventInfo = {}
 
 				const team1regx = /.+(?=\s\sv\s\s)/g;
@@ -50,65 +46,8 @@ module.exports = {
 
 		});
 
-
 		await browser.close();
 		console.log(data);
 		return data
 	},
 };
-
-
-
-
-// const crawlSkybet = (async () => {
-//
-// 	const browser = await puppeteer.launch({ headless: true});
-// 	const page = await browser.newPage();
-// 	page.setViewport({width: 1700, height: 1300});
-//
-// 	const baseURL = "https://m.skybet.com";
-// 	await page.goto(`${baseURL}/esports`);
-// 	// await page.waitForSelector('body.blog');
-// 	await page.addScriptTag({url: 'https://code.jquery.com/jquery-3.2.1.min.js'})
-// 	const csgoSelector = ' #page-content > .table-group > [data-ui-state=esportsfeatured] ';
-//
-// 	//get the entire dom as text
-// 	let allDom = await page.evaluate((sel) => {
-// 		let element = document.querySelector(sel);
-// 		return element ? element.innerHTML : null;
-// 	}, "body");
-//
-// 	const data = [];
-//
-//
-// 	const $ = cheerio.load(allDom)
-// 	$(csgoSelector).children().each((i, elem) => {
-// 		console.log(i)
-//
-// 		const eventName = $(elem).find('.accordion--generic > table > tbody').children().each((i,elem) => {
-// 			let eventInfo = {}
-//
-// 			const team1regx = /.+(?=\s\sv\s\s)/g;
-// 			const team2regx = /(?<=v ).+(?=(\s\())/g;
-//
-// 			eventInfo.eventName = $(elem).find('tr > .cell--link > a').attr('data-analytics');
-// 			eventInfo.pageHref = baseURL + $(elem).find('tr > .cell--link > a').attr('href')
-//
-// 			const teamsString = $(elem).find('tr > .cell--link > a').text().trim();
-// 			if(teamsString.match(team1regx).length === 1){
-// 				eventInfo.team1 = teamsString.match(team1regx)[0].trim()
-// 			}
-// 			if(teamsString.match(team2regx).length === 1){
-// 				eventInfo.team2 = teamsString.match(team2regx)[0].trim()
-// 			}
-//
-// 			data.push(eventInfo)
-// 		});
-//
-// 	});
-//
-//
-// 	await browser.close();
-// 	console.log(data);
-// 	return data
-// })();
