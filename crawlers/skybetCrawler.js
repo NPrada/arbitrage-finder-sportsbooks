57604e74291e4ffc721928e0e4dee53c236f13cb
NicoPr <niccolo@red7mobile.com>
2019-03-07 13:37:12 +0000

    reformat code using applyRegex

--- crawlers/skybetCrawler.js
+++ crawlers/skybetCrawler.js
@@ -36,35 +36,24 @@ module.exports = {
 				eventInfo.pageHref = baseURL + $(elem).find('tr > .cell--link > a').attr('href')
 
 				const fullEventName = $(elem).find('tr > .cell--link > a').attr('data-analytics');
-				//get the event name
-				if (fullEventName.match(eventNameRegx) !== null && fullEventName.match(eventNameRegx).length === 1) {
-					eventInfo.eventName = fullEventName.match(eventNameRegx)[0].trim()
-				} else {
-					console.error(`ERROR: some error with finding team1s name using ${eventNameRegx} from ${fullEventName}`);
-					eventInfo.error = `ERROR: some error with finding team1s name using ${eventNameRegx} from ${fullEventName}`
-				}
-				//get the sport name
-				if (fullEventName.match(sportNameRegx) !== null && fullEventName.match(sportNameRegx).length === 1) {
-					eventInfo.sportName = fullEventName.match(sportNameRegx)[0].trim()
-				} else {
-					console.error(`ERROR: some error with finding team1s name using ${sportNameRegx} from ${fullEventName}`);
-					eventInfo.error = `ERROR: some error with finding team1s name using ${sportNameRegx} from ${fullEventName}`
+				const teamsString = $(elem).find('tr > .cell--link > a').text().trim();
+				try{
+					eventInfo.eventName = applyRegex(fullEventName, eventNameRegx);//get the event name
+					eventInfo.sportName = applyRegex(fullEventName, sportNameRegx); //get the sport name
+					eventInfo.team1 = applyRegex(teamsString,team1regx);			// get the team1 name
+					eventInfo.team2 = applyRegex(teamsString,team2regx);			//get the team2 name
+				} catch (e) {
+					eventInfo.error = e;
 				}
 
-				const teamsString = $(elem).find('tr > .cell--link > a').text().trim();
-				// get the team1 name
-				if (teamsString.match(team1regx) !== null && teamsString.match(team1regx).length === 1) {
-					eventInfo.team1 = teamsString.match(team1regx)[0].trim()
-				} else {
-					console.error(`ERROR: some error with finding team1s name using ${team1regx} from ${teamsString}`);
-					eventInfo.error = `ERROR: some error with finding team1s name using ${team1regx} from ${teamsString}`
-				}
-				//get the team2 name
-				if (teamsString.match(team2regx) !== null &&teamsString.match(team2regx).length === 1) {
-					eventInfo.team2 = teamsString.match(team2regx)[0].trim()
-				} else {
-					console.error(`ERROR: some error with finding team2s name using ${team2regx} from ${teamsString}`);
-					eventInfo.error = `ERROR: some error with finding team1s name using ${team1regx} from ${teamsString}`
+
+				function applyRegex (string,regex){
+					if (string.match(regex) !== null && string.match(regex).length === 1) {
+						return string.match(regex)[0].trim()
+					} else {
+						throw `ERROR: some error with finding team2s name using ${team2regx} from ${teamsString}`;
+					}
+
 				}
 
 				data.push(eventInfo)
