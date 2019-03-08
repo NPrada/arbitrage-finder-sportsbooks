5ee73da20691505924a6d17a890b7381ea4df672
NicoPr <niccolo@red7mobile.com>
2019-03-08 16:54:37 +0000

    switched from pupetter to request

--- crawlers/skybetCrawler.js
+++ crawlers/skybetCrawler.js
@@ -1,23 +1,33 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+const request = require('request-promise-native');
 
 module.exports = {
 	run: async () => {
 
-		const browser = await puppeteer.launch({ headless: true});
-		const page = await browser.newPage();
-		page.setViewport({width: 1700, height: 1300});
+		// const browser = await puppeteer.launch({ headless: true});
+		// const page = await browser.newPage();
+		// page.setViewport({width: 1700, height: 1300});
+		//get the entire dom as text
+		// let allDom = await page.evaluate((sel) => {
+		// 	let element = document.querySelector(sel);
+		// 	return element ? element.innerHTML : null;
+		// }, "body");
+
 
 		const baseURL = 'https://m.skybet.com/';
-		await page.goto(`${baseURL}/esports`);
+		allDom = null;
 
-		//get the entire dom as text
-		let allDom = await page.evaluate((sel) => {
-			let element = document.querySelector(sel);
-			return element ? element.innerHTML : null;
-		}, "body");
+	 	await request({
+			url:`${baseURL}/esports`,
+			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'}, (error, response, html) => {
+			if(!error && response.statusCode){
+					console.log('done')
+					allDom = html
+			}
+		})
 
-		const $ = cheerio.load(allDom)
+		const $ = cheerio.load(allDom);
 		const eventDataSelector = ' #page-content > .table-group > [data-ui-state=esportsfeatured] ';
 		const matchDataSelector = '.accordion--generic > table > tbody';
 		const data = [];
@@ -38,10 +48,10 @@ module.exports = {
 				const fullEventName = $(elem).find('tr > .cell--link > a').attr('data-analytics');
 				const teamsString = $(elem).find('tr > .cell--link > a').text().trim();
 				try{
-					eventInfo.eventName = applyRegex(fullEventName, eventNameRegx);//get the event name
+					eventInfo.eventName = applyRegex(fullEventName, eventNameRegx);	//get the event name
 					eventInfo.sportName = applyRegex(fullEventName, sportNameRegx); //get the sport name
-					eventInfo.team1 = applyRegex(teamsString,team1regx);			// get the team1 name
-					eventInfo.team2 = applyRegex(teamsString,team2regx);			//get the team2 name
+					eventInfo.team1 = applyRegex(teamsString,team1regx);						// get the team1 name
+					eventInfo.team2 = applyRegex(teamsString,team2regx);						//get the team2 name
 				} catch (e) {
 					eventInfo.error = e;
 				}
@@ -60,7 +70,6 @@ module.exports = {
 			});
 		});
 
-		await browser.close();
 
 		if(data.length === 0){
 			console.log(allDom);
