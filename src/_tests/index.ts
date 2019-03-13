b164ed5fbd7b87adb19583da0df8541d02b5e027
NicoPr <niccolo@red7mobile.com>
2019-03-13 14:58:09 +0000

    add temporary tests folder for unit tests

---
+++ src/_tests/index.ts
@@ -0,0 +1,13 @@
+// standardiseSportName()
+
+// counter-strike
+// counter:strike
+// cs:go
+// counter_strike
+// csgo
+// csgo
+// counterstrikeglobaloffensive
+
+// counterstrikeglobaloffensiveoaishdoias
+// hellcsgolang
+// cshelloogo
