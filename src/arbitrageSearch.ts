5ac8fc47e4fbae93e895f8293a56fe75fa9e58f1
niccolo <niccolo.prada@gmail.com>
2019-03-14 19:13:10 +0000

    more robust search results report function

--- src/arbitrageSearch.ts
+++ src/arbitrageSearch.ts
@@ -30,23 +30,29 @@ export default class ArbSearch {
     
 
 		//TODO check if a single event matches to multiple events on the same sportsbook
-		console.log(this.buildResultsReport(matchesFound,sportbookIds))
+		console.log(this.buildResultsReport(matchesFound))
 	}
 	
-	buildResultsReport = (matchesFound:Array<{elem1: EventData, elem2: EventData}>, sportbookIds:Array<SportBookIds>): string => {
+	buildResultsReport = (matchesFound:Array<{elem1: EventData, elem2: EventData}>): string => {
+
+		let smallerResList: number | null= null;
+		let smallerSportsbook: SportBookIds | null = null;
 		
-		let smallerResList: number
-		let smallerSportsbook: SportBookIds;
-		if (this.allGamesCrawled[sportbookIds[0]].length < this.allGamesCrawled[sportbookIds[1]].length) {
-			smallerResList = this.allGamesCrawled[sportbookIds[0]].length
-			smallerSportsbook = sportbookIds[0]
-		}else{
-			smallerResList = this.allGamesCrawled[sportbookIds[1]].length
-			smallerSportsbook = sportbookIds[1]
-		}
-		const matchPercentage = Math.round((matchesFound.length/this.allGamesCrawled[smallerSportsbook].length)*10000)/100
+		const sportbookIds: Array<SportBookIds> = keys(this.allGamesCrawled) as Array<SportBookIds>
+
+		sportbookIds.map(sportbookName => {
+			if(isNil(smallerResList) || this.allGamesCrawled[sportbookName].length < smallerResList){
+				smallerResList = this.allGamesCrawled[sportbookName].length
+				smallerSportsbook = sportbookName
+			}
+		})
+		
+		if(smallerResList === null) smallerResList = 0
+
+		const matchPercentage = Math.round((matchesFound.length / smallerResList)*10000)/100
     return `Out of ${smallerResList} games on ${smallerSportsbook} we found ${matchesFound.length} matches ~${matchPercentage}%`
 	}
+
   isMatching = (match1:EventData, match2:EventData):boolean => {
     if (isNil(match1.eventName) || isNil(match2.eventName))
       return false
