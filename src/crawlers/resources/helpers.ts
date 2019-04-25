54f8598933605cbd57a731567130b4979e3988fd
niccolo <niccolo.prada@gmail.com>
2019-04-25 22:52:48 +0100

    tweak helpers

--- src/crawlers/resources/helpers.ts
+++ src/crawlers/resources/helpers.ts
@@ -3,7 +3,7 @@ import fs from 'fs'
 //just a helper if you want to log out html to a file for debugging purposes
 export const logHtml = (html: string | null | undefined) => {
   const fileName = 'HtmlLog.html'
-  fs.writeFile(fileName, html, function (err) {
+  fs.writeFile('debugging/'+fileName, html, function (err) {
     if (err) {
       return console.log(err);
     }
@@ -17,7 +17,7 @@ export const logHtml = (html: string | null | undefined) => {
  * @param fileName 
  */
 export const logJson = (json: object | null | undefined, fileName:string) => {
-   fs.writeFile(fileName+'.json', JSON.stringify(json), function (err) {
+   fs.writeFile('debugging/'+fileName+'.json', JSON.stringify(json), function (err) {
     if (err) {
       return console.log(err);
     }
@@ -41,13 +41,3 @@ export function parseHrtimeToSeconds(hrtime: Array<number>) {
     var seconds = (hrtime[0] + (hrtime[1] / 1e9)).toFixed(3);
     return seconds;
 }
-
-/**
- * 
- * @param min 
- * @param max 
- */
-export function getRandomArbitrary(min:number, max:number):number {
-  return Math.random() * (max - min) + min;
-}
-
