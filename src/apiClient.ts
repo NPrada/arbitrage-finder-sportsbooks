9416320f03e0120cc955b4feb3dcf13908c9332c
niccolo <niccolo.prada@gmail.com>
2019-06-20 08:57:44 +0100

    add complete apiClient

---
+++ src/apiClient.ts
@@ -0,0 +1,22 @@
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+import 'cross-fetch/polyfill';
+
+export const postFullCrawlObject = async (input: any) => {
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql"
+});
+
+
+return await client.mutate({
+  mutation: gql`mutation insertJob($input: CrawlResultInput!) {
+      createCrawlResultContainer(input: $input ) {
+          id
+      }
+  }`,
+  variables: {
+    input: input
+  }
+})
+	.then(result => console.log(result));
+}
